8cf6823999f75775a7d10ca22eb34a5830c3d75f
yokeshthirumoorthi <yokeshthirumoorthi@gmail.com>
2018-09-06 12:54:05 -0700

    add copyright and credits

--- src/schema.rs
+++ src/schema.rs
@@ -1,3 +1,13 @@
+// Copyright © 2018 Yokesh Thirumoorthi
+// [This program is licensed under the "MIT License"]
+// Please see the file LICENSE in the source
+// distribution of this software for license terms.
+
+// CREDITS
+// Project: https://github.com/actix/examples/tree/master/diesel
+// Copyright (c) 2017 Nikolay Kim (fafhrd91@gmail.com)
+// License (MIT) https://github.com/actix/examples/blob/master/LICENSE
+
 table! {
     users (id) {
         id -> Text,
